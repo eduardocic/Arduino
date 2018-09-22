f526bab3ab990e664a0dcb1aca76a2fbb160f268
Eduardo <eduardo.ime12@gmail.com>
2018-09-22 19:26:18 -0300

    Utilizando o TIMER 1. Dessa forma que tenho menos interrupção e com isso eu não atraso tanto o meu relógio.

--- LCD_TIMER1_Irriga/LCD_TIMER1_Irriga.ino
+++ LCD_TIMER1_Irriga/LCD_TIMER1_Irriga.ino
@@ -1,7 +1,7 @@
 /*********************************************************
  * 
  * Autor: Eduardo H. Santos.
- * Data:  06 de Setembro de 2018
+ * Data:  22 de Setembro de 2018
  * 
  * 
  * Resumo: 
@@ -14,47 +14,36 @@
  * Explicação:
  * 
  *   1. Farei a programação para que haja a interrupções
- *      no TIMER0 de forma a atualizar as variáveis a 
+ *      no TIMER1 de forma a atualizar as variáveis a 
  *      cada 1s. Isso me dará uma boa base temporal para
  *      a confecção da lógica que se dará para aguar o
  *      sistema uma vez por dia durante um determinado
  *      período de tempo.
  *      
- *   2. A interrupção por TIMER0 neste caso ocorrerá 
- *      quando o valor do registrador TCNT0 (o qual será
- *      atualizado a cada 'clock' do sistema) for igual
- *      ao valor salvo no registrador OCR0A.
+ *   2. A interrupção por TIMER1 neste caso ocorrerá 
+ *      quando o valor do registrador TCNT1 atinja 0xFFF;
  *   
  *   3. Eu setei um prescale para o meu uC de 1024. Dessa     
- *      forma o TCNT0 terá a atualização de valor de 
+ *      forma o TCNT1 terá a atualização de valor de 
  *      forma incremetal quando o Cristal interno oscilar
  *      1024 vezes. Como o Cristal externo é de 16MHz, 
- *      significa fizer que o TCNT0 será incrementado na 
+ *      significa fizer que o TCNT1 será incrementado na 
  *      taxa de (16 MHz)/(1024) = 15.625 Hz. 
  *   
- *   4. A frequência do TCNT0 sendo 15.625 Hz significa
+ *   4. A frequência do TCNT1 sendo 15.625 Hz significa
  *      dizer que a cada 0,000064s haverá um incremento
- *      no TCNT0. Para eu ter a bumbo de 1s de precisão,
+ *      no TCNT1. Para eu ter a bumbo de 1s de precisão,
  *      eu preciso trabalhar em cima deste tempo de forma
  *      a atualizar o meu sistema nessa taxa.
  *      
  *   5. Criarei uma variável do tipo 'static int' de for-
  *      ma a ser atualizara a cada interrupção por timer.
- *   
- *   6. Pelas relações matemáticas, se eu multiplicar
- *      0,000064s por 125 e depois por 125 novamente, eu
- *      terei precisamente 1s. 
  *      
- *   7. Assim, inicialmente eu inicializarei o OCR0A com
- *      o valor de 125. Com isto, a cada 125*0,000064s
- *      ocorrerá uma interrupção por TIMER0. Com isto, 
- *      a interrupção ocorrerá a cada 0,008s.
- *      
- *   8. Atualizarei, então, a variável 'static int' até
- *      atingir o valor de 125. Isto me dará um bumbo de 
- *      1s de precisão.
- *      
- *   9. Apresentarei os valores das horas no display de 
+ *   6. Assim, inicialmente eu inicializarei o TCNT1 com
+ *      o valor de 0xC2F7. Com isto, a cada 1s ocorrerá 
+ *      uma interrupção por overflow do TIMER1.
+ *  
+ *   7. Apresentarei os valores das horas no display de 
  *      LCD.
  ********************************************************/
 #include <LiquidCrystal.h>
@@ -70,12 +59,11 @@
 #define LED  13
 #define RELE 10
  
-static int cont    = 0;
 static int segundo;
 static int minuto;
 static int hora;
 static int dia;
-int        T = 4;  // Tempo de acionamento do relé.
+int        T = 4;  // Tempo de acionamento do relé, em s.
 
 static char stringHora[10];
 
@@ -115,7 +103,7 @@ LiquidCrystal Display_LCD(2, 3, 4, 5, 6, 7);     // Funcionou beleza.
  *  
  ********************************************************/
 void initTime(int _dia, int _hora, int _minuto, int _segundo);
-void configTimer0();
+void configTimer1();
 void imprimeSerial();
 void imprimeLCD();
 
@@ -129,7 +117,7 @@ void setup() {
   pinMode(LED, OUTPUT);
   pinMode(RELE, OUTPUT);
     
-  configTimer0();
+  configTimer1();
   initTime(0, 6, 59, 50);
   
   Serial.begin(9600);
@@ -152,54 +140,34 @@ void loop() {
  *  Função de tratamento de interrupção
  *  
  ********************************************************/
-ISR(TIMER0_COMPA_vect)
+ISR(TIMER1_OVF_vect)
 {
+   TCNT1 = 0xC2F7;     
+   segundo++;
    
-   /* Atualizado a cada 0,008s. */
-   cont++;
-
-   /* Este evento acontece apenas quando se completa 1s. */
-   /* if(cont == 125){ // Por experiência empírica, eu vi que atrasa muito com 125. */
-   if(cont == 124){
-     cont = 0;
-     segundo++;
-
-     if ( segundo == 60 ){
-       minuto++;
-       segundo = 0;
-       if ( minuto == 60 ){
-         hora++;
-         minuto = 0;
-         if ( hora == 24 ){
-           dia++;
-           hora = 0;
-         }
+   if ( segundo == 60 ){
+     minuto++;
+     segundo = 0;
+     if ( minuto == 60 ){
+       hora++;
+       minuto = 0;
+       if ( hora == 24 ){
+         dia++;
+         hora = 0;
        }
      }
-
-
-     if ( (hora == 7) && (minuto == 0) && (segundo == 0) ){
-        digitalWrite(RELE, HIGH);
-     }
-     if ( (hora == 7) && (minuto == 0) && (segundo == T) ){
-        digitalWrite(RELE, LOW);
-     }
-
-  
-//     static int flag = 0;   
-//     /* Alerta por LED para acompanhamento da atividade. */
-//     if (flag == 0){
-//       digitalWrite(LED, HIGH);    
-//       flag = 1;
-//     }  
-//     else{
-//       digitalWrite(LED, LOW);    
-//       flag = 0;
-//     }
-
-     /* Imprime a hora no display serial */
-     imprimeLCD();
    }
+
+
+   if ( (hora == 7) && (minuto == 0) && (segundo == 0) ){
+      digitalWrite(RELE, HIGH);
+   }
+   if ( (hora == 7) && (minuto == 0) && (segundo == T) ){
+      digitalWrite(RELE, LOW);
+   }
+
+   /* Imprime a hora no display serial */
+   imprimeLCD();
 }
 
 
@@ -208,12 +176,11 @@ ISR(TIMER0_COMPA_vect)
  *  Demais funções do Sistema.
  *  
  ********************************************************/
-
-void configTimer0(){
+void configTimer1(){
   /* Inicialmente eu faço o reset de todos os registradores. */
-  TCCR0A = 0x00;
-  TCCR0B = 0x00;
-  TCNT0  = 0x00;
+  TCCR1A = 0x00;
+  TCCR1B = 0x00;
+  TCNT1  = 0xC2F7;
 
   /********************************************************************* 
    * 1. Coloco o OCR0A como 125. Este valor em hexa é 0x7D.
@@ -225,10 +192,9 @@ void configTimer0(){
    *    que  eu tenha interrupção quando ocorrer a interrupção
    *    por igualdade entre o TCNT0 com o OCR0A.             
    ********************************************************************/
-  OCR0A  = 0x7D;
-  TCCR0A = 0x02;
-  TCCR0B = 0x05;
-  TIMSK0 = 0x02;
+  TCCR1A = 0x00;
+  TCCR1B = 0x05;
+  TIMSK1 = 0x01;
 }
 
 
